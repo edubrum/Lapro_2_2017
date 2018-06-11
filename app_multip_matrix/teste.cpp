bc92a4ce4cac98932ecfdc45cb0f1d141fa2e229
edubrum <edulbrum@gmail.com>
2018-06-11 17:07:16 -0300

    a

--- app_multip_matrix/teste.cpp
+++ app_multip_matrix/teste.cpp
@@ -6,7 +6,8 @@ void flush(){
 		cout<<endl;	
 }
 int main(void){
-	int i,j,l,c,u_A,v_A,u_B,v_B,**A,**AT,**B,**C,entra_num;
+	int i,j,l,c,u_A,v_A,u_B,v_B;
+	float **A,**AT,**B,**C,**A_inv,entra_num;
 	//u = tamanho fixo, podendo ser infromado pelo usuário, das linhas
 	//v = tamanho fixo, podendo ser informado pelo usuário, das colunas
 	flush();
@@ -16,37 +17,39 @@ int main(void){
 	u_A=v_B=2;
 	i=j=l=c=0;
 	//criação das matriz por alocação dinamica
-	A = new int*[u_A];
-	AT= new int*[v_A];
-	B = new int*[u_B];
-	C = new int*[u_A];
+	A = 	new float*[u_A];
+	AT= 	new float*[v_A];
+	B = 	new float*[u_B];
+	C = 	new float*[u_A];
+	A_inv = new float*[2];
 	for (i = 0; i < u_A ; i++){
-		A[i] = new int[v_A];
-		C[i] = new int[v_B];
+		A[i] = new float[v_A];
+		C[i] = new float[v_B];
+		A_inv[i] = new float[2];
 
 	}
 	for( i = 0 ; i < u_B;i++){
-	B[i] = new int[v_B];
-	AT[i]= new int[u_A];
+		B[i] = new float[v_B];
+		AT[i]= new float[u_A];
 	}
 	//as três matrizes serão límpas:
-	for(i=0;i<u_A;i++){
-		for(j=0;j<v_A;j++){
+	for(i=0;i<u_A;i++)
+		for(j=0;j<v_A;j++)
 			A[i][j]=0;
-		}
-	}
 	for(i=0;i<u_B;i++){
 		for(j=0;j<v_B;j++){
 			B[i][j]=0;
 			AT[i][j]=0;
 		}
 	}
-	for(i=0;i<u_A;i++){
-		for(j=0;j<v_B;j++){
+	for(i=0;i<u_A;i++)
+		for(j=0;j<v_B;j++)
 			C[i][j]=0;
-		}
-	}
+	for(i=0;i<2;i++)
+		for(j=0;j<2;j++)
+			A_inv[i][j]=0;
 	flush();
+	//printando as matrizes zeradas
 	cout<<"Inicialmente as matrizes estão zeradas:"<<endl<<endl<<" A =";
 	for(l=0;l<u_A;l++){
 		for(c=0;c<v_A;c++){
@@ -93,7 +96,25 @@ int main(void){
 		}
 		cout<<endl;
 	}
-	cout<<endl;
+	if(u_A==v_A){
+		cout<<endl<<" A_inv =";
+		for(l=0;l<2;l++){
+			for(c=0;c<2;c++){
+				if((c==0)&&(l==1))
+					cout<<"        |"<<A_inv[l][c]<<" ";
+				else{
+					if(c==0)
+						cout<<"|"<<A_inv[l][c]<<" ";
+					else
+						cout<<A_inv[l][c]<<"|";
+				}
+			}
+			cout<<endl;
+		}
+		cout<<endl;
+	}
+	else
+		cout<<endl<<" Como A não é matriz quadrada 2x2, não é possível fazer sua inversa neste programa."<<endl;
 	//aqui começa a inserção de valores nas matrizes;
 	for(i=0;i<u_A;i++){
 		for(j=0;j<v_A;j++){
@@ -196,9 +217,38 @@ int main(void){
 				cout<<endl;
 			}
 	cout<<endl;
-	//lembrando que:
-	//u = tamanho fixo, podendo ser infromado pelo usuário, das linhas
-	//v = tamanho fixo, podendo ser informado pelo usuário, das colunas
+	//printando novamente A
+	cout<<endl<<"Você criou as matrizes:"<<endl<<endl<<" A =";
+			for(l=0;l<u_A;l++){
+				for(c=0;c<v_A;c++){
+					if((c==0)&&(l==1))
+						cout<<"    |"<<A[l][c]<<" ";
+					else{
+						if((c==0)&&(l==0))
+							cout<<"|"<<A[l][c]<<" ";
+						else{
+							if(c== (v_A - 1))
+								cout<<A[l][c]<<"|";
+							else
+								cout<<A[l][c]<<" ";
+						}
+					}
+				}
+				cout<<endl;
+			}
+	//Calculando na inversa de A:
+	for(i=0;i<2;i++){
+		for(j=0;j<2;j++){
+			if(i==0){
+				A_inv[i][0]=A[1][1];
+				A_inv[i][1]= -(A[j][i]);
+			}
+			else{
+				A_inv[i][0]= -(A[j][i]);
+				A_inv[i][1]=A[0][0];
+			}
+		}
+	}
 	//multiplicando -> A*B =C
 	for(i=0;i<u_A;i++){
 		for(j=0;j<v_A;j++){
@@ -212,7 +262,25 @@ int main(void){
 			}
 		}
 	}
-	//printando o resultado
+	//printando a inversa de A
+	if(u_A==v_A){
+	cout<<endl<<" A_inv =";
+		for(l=0;l<2;l++){
+			for(c=0;c<2;c++){
+				if((c==0)&&(l==1))
+					cout<<"        |"<<A_inv[l][c]<<" ";
+				else{
+					if(c==0)
+						cout<<"|"<<A_inv[l][c]<<" ";
+					else
+						cout<<A_inv[l][c]<<"|";
+				}
+			}
+			cout<<endl;
+		}
+		cout<<endl;
+	}
+	//printando o resultado da multiplicação
 	cout<<endl<<"A multiplicação de A por B é"<<endl<<endl<<" C =";
 			for(l=0;l<u_A;l++){
 				for(c=0;c<v_B;c++){
@@ -228,10 +296,11 @@ int main(void){
 				cout<<endl;
 			}
 	cout<<endl;
-	//apaga as matrizes A, B e C alocadas, para não estourar a memória
+	//apaga as matrizes A, AT, A_inv,B e C alocadas, para não estourar a memória
 	for (i = 0; i < u_A; i++){
 		delete [] A[i];
 		delete [] C[i];
+		delete [] A_inv[i];
 		
 	}
 	for(i = 0; i< u_B ; i++){
@@ -240,6 +309,7 @@ int main(void){
 	}
 	delete [] A;
 	delete [] AT;
+	delete [] A_inv;
 	delete [] B;
 	delete [] C;
 	return 0;
